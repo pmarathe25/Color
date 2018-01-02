90af07fb5bb8ca62f61b8d2bd2fc936af248a530
pmarathe25 <pmarathe25@gmail.com>
2018-01-01 21:45:35 -0500

    Adds uninstallation script

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 DIR="$(dirname "$(readlink -f "$0")")"
 sudo ln -snf $DIR/include /usr/local/include/Color
 make lib
-sudo ln -s $DIR/lib/* /usr/local/lib/
+sudo ln -sf $DIR/lib/libstealthcolor.so /usr/local/lib/libstealthcolor.so
